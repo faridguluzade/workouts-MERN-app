2ca63f9a286557d87a72920be507e23f0eb6e972
faridguluzade <tu5mhbxzt.code.edu.az>
2023-08-02 21:47:56 -0800

    adding delete future

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,20 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const res = await fetch(`/api/workouts/${workout._id}`, {
+      method: "DELETE",
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: data });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
