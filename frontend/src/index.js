ae4ed5ae5ee8c7c340b7402135e30fa57c54033e
faridguluzade <tu5mhbxzt.code.edu.az>
2023-08-03 20:06:58 -0800

    creating auth context and as well as login & signup component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 
 import { WorkoutsProvider } from "./context/WorkoutsContext";
+import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkoutsProvider>
-      <App />
-    </WorkoutsProvider>
+    <AuthProvider>
+      <WorkoutsProvider>
+        <App />
+      </WorkoutsProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
