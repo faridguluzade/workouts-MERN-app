2389950c09416c0ca2a4e3c5a0ca62b33af65707
faridguluzade <tu5mhbxzt.code.edu.az>
2023-08-03 19:33:20 -0800

    adding token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,6 +17,7 @@ const userSchema = new Schema({
   },
 });
 
+// static signup method
 userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
     throw new Error("All fields must be filled");
@@ -40,6 +41,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw new Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw new Error("Incorrect password");
+  }
 
   return user;
 };
